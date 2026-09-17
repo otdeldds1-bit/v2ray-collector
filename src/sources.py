89816a519d3c9bac1bb9669568b6881ec0f8b30c
otdeldds1-bit <otdeldds1@gmail.com>
2026-09-17 07:44:00 +0300

    Create sources.py

---
+++ src/sources.py
@@ -0,0 +1,15 @@
+# Публичные источники подписок (VLESS / VMess / Trojan / SS).
+# Список можно свободно расширять — добавь свои любимые репозитории.
+SOURCES = [
+    "https://raw.githubusercontent.com/barry-far/V2ray-Configs/main/All_Configs_Sub.txt",
+    "https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.txt",
+    "https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2",
+    "https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub",
+    "https://raw.githubusercontent.com/ALIILAPRO/v2rayNG-Config/main/server.txt",
+    "https://raw.githubusercontent.com/freefq/free/master/v2",
+    "https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray",
+    "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v2ray.txt",
+]
+
+TEST_URL = "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg"  # маленькая картинка с YouTube CDN
+TEST_URL_FALLBACK = "https://www.youtube.com/"
