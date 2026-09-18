80f8ff9fc4edb645767f0c656e551f6feaeb3477
otdeldds1-bit <otdeldds1@gmail.com>
2026-09-18 03:41:04 +0300

    Update sources.py

--- src/sources.py
+++ src/sources.py
@@ -2,22 +2,7 @@
 # Формат ссылок с эмодзи-флагами сохраняется.
 
 SOURCES = [
-    "https://raw.githack.com/igareck/vpn-configs-for-russia/main/BLACK_VLESS_RUS_mobile.txt",
-    "https://raw.githack.com/igareck/vpn-configs-for-russia/main/BLACK_VLESS_RUS.txt",
-    "https://raw.githack.com/igareck/vpn-configs-for-russia/main/Vless-Reality-White-Lists-Rus-Mobile.txt",
-    "https://raw.githack.com/igareck/vpn-configs-for-russia/main/WHITE-CIDR-RU-all.txt",
-    "https://raw.githubusercontent.com/MahanKenway/Freedom-V2Ray/main/configs/mix.txt",
-    "https://raw.githubusercontent.com/Surfboardv2ray/TGParse/main/splitted/mixed",
-    "https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/Eternity.txt",
-    "https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2",
-    "https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub",
-    "https://raw.githubusercontent.com/ALIILAPRO/v2rayNG-Config/main/server.txt",
-    "https://raw.githubusercontent.com/freefq/free/master/v2",
-    "https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray",
-    "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v2ray.txt",
-    "https://raw.githubusercontent.com/MrPooyaX/SansorchiFucker/main/data.txt",
-    "https://raw.githubusercontent.com/Epodonios/v2ray-configs/main/All_Configs_Sub.txt",
-    "https://raw.githubusercontent.com/roosterkid/openproxylist/main/V2RAY_RAW.txt",
-]
+  
+    "https://gitlab.com/solovyov-jenya2004/all_subs/-/blob/main/final_sorted?ref_type=heads"
 
 TEST_URL = "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg"
