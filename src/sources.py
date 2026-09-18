b4f7fdd11eac370590e964648d3333a2829fff6a
otdeldds1-bit <otdeldds1@gmail.com>
2026-09-18 05:26:53 +0300

    Update sources.py

--- src/sources.py
+++ src/sources.py
@@ -1,10 +1,5 @@
 SOURCES = [
-    # === Главный источник: all_subs (тестируется автором из РФ) ===
-    "https://gitlab.com/solovyov-jenya2004/all_subs/-/raw/main/final_sorted",
-
-    # === Дополнительные РФ-источники ===
-    "https://raw.githack.com/igareck/vpn-configs-for-russia/main/BLACK_VLESS_RUS_mobile.txt",
-    "https://raw.githack.com/igareck/vpn-configs-for-russia/main/BLACK_VLESS_RUS.txt",
+   https://raw.githubusercontent.com/kort0881/vpn-vless-configs-russia/main/githubmirror/ru-sni/vless_ru.txt,
 ]
 
 TEST_URL = "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg"
