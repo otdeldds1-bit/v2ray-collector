89ce92e4e829560f00f54ccb6cb3c40fd9fa1051
otdeldds1-bit <otdeldds1@gmail.com>
2026-09-18 03:48:51 +0300

    Update sources.py

--- src/sources.py
+++ src/sources.py
@@ -3,6 +3,6 @@
 
 SOURCES = [
   
-    "https://gitlab.com/solovyov-jenya2004/all_subs/-/blob/main/final_sorted?ref_type=heads"
+    "https://gitlab.com/solovyov-jenya2004/all_subs/-/raw/main/final_sorted?ref_type=heads"
 ]
 TEST_URL = "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg"
