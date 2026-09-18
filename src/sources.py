e52aea6d41a35068ca1b0517ce5df85bb5a47248
otdeldds1-bit <otdeldds1@gmail.com>
2026-09-18 05:38:12 +0300

    Update sources.py

--- src/sources.py
+++ src/sources.py
@@ -1,6 +1,7 @@
 SOURCES = [
    
-   https://raw.githubusercontent.com/kort0881/vpn-vless-configs-russia/refs/heads/main/output/vless.txt
+    " https://raw.githubusercontent.com/kort0881/vpn-vless-configs-russia/refs/heads/main/output/vless.txt",
+  
 ]
 
 TEST_URL = "https://i.ytimg.com/vi/dQw4w9WgXcQ/hqdefault.jpg"
